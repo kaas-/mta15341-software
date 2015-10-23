e72c5cc3b8e5a5fc3667e18d7402dc9f463bb72b
kaas- <rall94.thekaas@gmail.com>
2015-10-23 14:42:49 +0200

    set up interfaces for Hex and BLOB classes

---
+++ TouchBoard/BLOB.h
@@ -0,0 +1,24 @@
+#ifndef BLOB_H
+#define BLOB_H
+#endif
+
+#include "opencv2/opencv.hpp"
+
+class BLOB
+{
+private:
+	cv::Mat src;
+	cv::Mat thisBLOB;
+
+	cv::Mat getBLOBFromImage(cv::Mat s);
+	void setBLOB(cv::Mat newBLOB);
+	void setSrc(cv::Mat s) { src = s; };
+
+	BLOB() { }
+public:
+	BLOB(cv::Mat s);
+
+	cv::Mat getBLOBImage() { return thisBLOB; };
+	double compareTo(BLOB otherBLOB);
+};
+
---
+++ TouchBoard/Hex.h
@@ -0,0 +1,21 @@
+#ifndef HEX_H
+#define HEH_H
+#endif
+
+#include <array>
+
+class Hex 
+{
+private:
+	std::array<int, 2> position;
+	int colour;
+	void setPosition(std::array<int, 2> p) { position = p; };
+	Hex() { }
+	
+public:
+	Hex(std::array<int, 2> p);
+	void setColour(int c) { colour = c; };
+	int getColour() { return colour; };
+	std::array<int, 2> getPosition() { return position; };
+
+};
