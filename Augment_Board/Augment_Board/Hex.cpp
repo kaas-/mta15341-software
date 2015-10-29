e8fdb6f1a2f42543145445c5c27d0e7017bbcf4d
Mark <eutrapely@gmail.com>
2015-10-29 11:37:27 +0100

    started colour designation switch - needs to be finished

--- Augment_Board/Augment_Board/Hex.cpp
+++ Augment_Board/Augment_Board/Hex.cpp
@@ -4,6 +4,18 @@
 Hex::Hex(Tile::Type t, unsigned long int c)
 	: colour(c), TileType(t)
 {
+	switch (TileType)
+	{
+	case Tile::Type::BADLANDS:
+		colour = 0xff0000; //RED
+		break;
+	case Tile::Type::LAKES: 
+		colour = 0x0066FF;
+		break;
+	default:
+		colour = 0xFFFFFF;
+		break;
+	}
 }
 
 
@@ -20,3 +32,6 @@ void Hex::setColour(unsigned long int c)
 {
 	colour = c;
 }
+
+
+
