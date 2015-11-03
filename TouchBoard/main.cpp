8f053bdadfc2c6e8d2049efb6d94490c702be1a7
Rasmus Christiansen <rall94.thekaas@gmail.com>
2015-11-03 12:57:10 +0100

    added segment namespace to augment board project

--- TouchBoard/main.cpp
+++ TouchBoard/main.cpp
@@ -7,7 +7,7 @@ using namespace Segment;
 
 int main(int, char)
 {
-	VideoCapture cap(0); // open the default camera
+	VideoCapture cap(1); // open the default camera
 	if (!cap.isOpened()) // check if we succeeded
 		return -1;
 	Mat edges;
@@ -16,9 +16,9 @@ int main(int, char)
 	{
 		Mat frame;
 		cap >> frame; // get a new frame from camera
-		imshow("color", frame);
+		//imshow("color", frame);
 		frame = rgb2gray(frame);
-		imshow("gray", frame);
+		//imshow("gray", frame);
 		GaussianBlur(frame, frame, Size(7, 7), 1.5, 1.5);
 		frameOtsu = getHistogram(frame);
 		frame = thresholdImg(frame, getBestOtsuScore(frameOtsu));
