2bdd7375bd457a9727ffe65655b2d7275686ff9f
Mark <eutrapely@gmail.com>
2015-11-24 10:27:56 +0100

    Messing with runWebcam and addWeighted

--- Augment_Board/Augment_Board/main.cpp
+++ Augment_Board/Augment_Board/main.cpp
@@ -2,6 +2,7 @@
 #include "Player.h"
 #include "Board.h"
 #include "Segment.h"
+#include <vector>
 
 using namespace cv;
 using namespace std;
@@ -21,6 +22,8 @@ Hex findCurrentHex(Board currentBoard);
 void changeCurrentColour(Hex hex, Player player);
 
 int runWebcam();
+
+Mat getWeightedFrames(VideoCapture cap, Mat firstFrame, Mat nextFrame, Mat weightedFrame, int duration);
 
 int main(int, char)
 {
@@ -53,10 +56,10 @@ int main(int, char)
 	waitKey(0);
 
 	return 0;*/
-	//runWebcam();
+	runWebcam();
 
-	board.buildBoard();
-	board.drawBoard();
+	//board.buildBoard();
+	//board.drawBoard();
 
 	cin.get();
 	cin.get();
@@ -65,39 +68,59 @@ int main(int, char)
 
 int runWebcam()
 {
-	VideoCapture cap(1); // open the default camera
+	VideoCapture cap(0); // open the default camera
 	if (!cap.isOpened()) // check if we succeeded
 		return -1;
+	Mat frame;
+	Mat frame2;
+	Mat finalFrame;
 	Mat gblur;
 	Mat thres1;
 	Mat thres2;
-	Mat eroded;
+	Mat median;
+	Mat eroded1;
+	Mat eroded2;
+	Mat frameArray;
+	
+	finalFrame = getWeightedFrames(cap, frame, frame2, finalFrame, 60);
+	//cout << frameArray.size();
+
 	for (;;)
 	{
-		Mat frame;
-		cap >> frame; // get a new frame from camera
-		cvtColor(frame, frame, CV_BGR2GRAY);
+		Mat current;
+		Mat dst;
+		Mat finaldst;
+		cap >> current; // get a new frame from camera
+		cvtColor(current, current, CV_BGR2GRAY);
+		/*Scalar tempVal = mean(frameArray);
+		float myMatatMean = tempVal.val[0];*/
+		absdiff(current, finalFrame, dst);
+		//absdiff(myMatatMean, dst, dst);
+		//absdiff(current, dst, finaldst);
+		//absdiff(current, firstFrame, dst);
+		//absdiff(firstFrame, dst, dst);
+		//absdiff(current, dst, finaldst);
 		//GaussianBlur(frame, blur, Size(19, 19), 1.5, 1.5);
 		//threshold(blur, thres, 25, 255, THRESH_BINARY);
-		//GaussianBlur(frame, gblur, Size(5, 5), 1.5, 1.5);
-		//blur(frame, gblur, Size(5, 5), Point(1, -1));
-		bilateralFilter(frame, gblur, 5, 5 * 2, 5 / 2);
-		//threshold(blur, thres, 0, 255, THRESH_BINARY | THRESH_OTSU);
-		threshold(gblur, gblur, 15, 255, THRESH_BINARY);
-		adaptiveThreshold(gblur, thres1, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 111, 0);
-		//medianBlur(frame, frame, 3);
+		GaussianBlur(dst, gblur, Size(5, 5), 1.5, 1.5);
+		//blur(finaldst, gblur, Size(3, 3), Point(1, -1));
+		inRange(gblur, Scalar(21), Scalar(100), thres2);
+		
+		//threshold(gblur, thres1, 15, 255, THRESH_BINARY);
+		//adaptiveThreshold(gblur, thres1, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 111, 0);
+		//medianBlur(thres2, median, 5);
 		//cv::erode(thres, eroded, element);
-		morphologyEx(thres1, eroded, MORPH_OPEN, element);
+		morphologyEx(thres2, eroded1, MORPH_OPEN, element, Point(-1, -1), 1);
+		//morphologyEx(thres1, eroded, MORPH_CLOSE, element, Point(-1, -1), 1);
 		//medianBlur(eroded, eroded, 13);
-		morphologyEx(eroded, eroded, MORPH_OPEN, element);
-		//morphologyEx(eroded, eroded, MORPH_CLOSE, element);
-		//morphologyEx(eroded, eroded, MORPH_CLOSE, element);
-		//morphologyEx(eroded, eroded, MORPH_CLOSE, element);
-		//morphologyEx(eroded, eroded, MORPH_CLOSE, element);
-		imshow("orig-grey", frame);
-		imshow("blur", gblur);
-		imshow("mean", thres1);
-		imshow("gauss", eroded);
+		imshow("current", current);
+		imshow("subtracted", dst);
+		//imshow("dst", finaldst);
+		//imshow("blur", gblur);
+		//imshow("mean", thres2);
+//		imshow("median", median);
+		//imshow("morph1", eroded1);
+		//imshow("morph2", eroded2);
 		if (waitKey(30) >= 0)
 			break;
 	}
@@ -113,4 +136,23 @@ int runWebcam()
 void changeCurrentColour(Hex hex, Player player)
 {
 
+}
+
+Mat getWeightedFrames(VideoCapture cap, Mat firstFrame, Mat nextFrame, Mat weightedFrame, int duration)
+{
+	cap >> firstFrame;
+	nextFrame = firstFrame.clone();
+	weightedFrame = firstFrame.clone();
+	cap.grab();
+	cap.retrieve(nextFrame);
+	cvtColor(firstFrame, firstFrame, CV_BGR2GRAY);
+	cvtColor(nextFrame, nextFrame, CV_BGR2GRAY);
+	addWeighted(firstFrame, 0.5, nextFrame, 0.5, 0.0, weightedFrame);
+
+	for (int i = 0; i < duration; i++)
+	{
+		getWeightedFrames(cap, firstFrame, weightedFrame, weightedFrame, duration);
+		cout << "Weighing frames...";
+	}
+	return weightedFrame;
 }
